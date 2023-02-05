e5e94216fe92e5b6657d2bbcaa860aa3639aae93
Stevo <118697920+stevenull@users.noreply.github.com>
2023-02-05 21:55:25 +1100

    Updated config.lua
    Fixed Duration being 300k millisecs by default

--- stevo_moneywash/shared/config.lua
+++ stevo_moneywash/shared/config.lua
@@ -5,4 +5,4 @@ Config.Locale           = 'en'
 
 Config.cooldown = 600000 -- Cooldown before player can wash again. 60 seconds by default.
 
-Config.washduration = 300000 -- Time it takes player to wash money. 30 seconds by default.
+Config.washduration = 30000 -- Time it takes player to wash money. 30 seconds by default.
