00368456b310dac2fcae2c9cffa7417630e128b1
Stevo <118697920+stevenull@users.noreply.github.com>
2023-02-07 09:45:46 +1100

    Update config.lua

--- stevo_moneywash/shared/config.lua
+++ stevo_moneywash/shared/config.lua
@@ -1,8 +1,8 @@
 Config  = {}
 
-Config.Locale           = 'en'
 
+Config.Cooldown = 600000 -- Cooldown before player can wash again. 60 seconds by default.
 
-Config.cooldown = 600000 -- Cooldown before player can wash again. 60 seconds by default.
+Config.WashDuration = 30000 -- Time it takes player to wash money. 30 seconds by default.
 
-Config.washduration = 30000 -- Time it takes player to wash money. 30 seconds by default.
+Config.TaxRate = 0.10 -- The Amount Taxed from players when using the moneywash, by default it will take 10%
